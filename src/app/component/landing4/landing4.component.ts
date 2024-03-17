1c0925e9dd521593db90f2d26f81192b72ae675b
raki <mohammed.raki26@gmail.com>
2024-03-18 00:48:16 +0100

    effect

--- src/app/component/landing4/landing4.component.ts
+++ src/app/component/landing4/landing4.component.ts
@@ -1,12 +1,37 @@
+import { NgClass } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-landing4',
   standalone: true,
-  imports: [],
+  imports: [NgClass],
   templateUrl: './landing4.component.html',
   styleUrl: './landing4.component.css'
 })
 export class Landing4Component {
+  text = 'Creative'; // Texte initial
+  texts = ['Creative', 'Confiance', 'Fort'];
+  currentIndex = 0;
+  activateEffect = false;
+  // private intervalId: any;
+
+  constructor() { 
+// Change le texte toutes les secondes
+    setInterval(() => {
+      this.activateEffect = true
+      
+    }, 2900);
+
+    setInterval(() => {
+
+      this.text = this.texts[this.currentIndex];
+      if(this.currentIndex + 1 == this.texts.length){
+        this.currentIndex = 0;
+      }else{
+        this.currentIndex = this.currentIndex + 1 ;
+      }
+      this.activateEffect = false;
+    }, 3000); 
+  }
 
 }
