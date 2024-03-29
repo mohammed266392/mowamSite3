86f71677a704ab3f5c0b50b519506a9f19dcf516
raki <mohammed.raki26@gmail.com>
2024-03-29 12:05:13 +0100

    Footer

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,11 +3,12 @@ import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './component/header/header.component';
 import { Header2Component } from './component/header2/header2.component';
 import { OngletService } from './services/onglet.service';
+import { Footer2Component } from './component/footer2/footer2.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, Header2Component],
+  imports: [RouterOutlet, Header2Component, Footer2Component],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
