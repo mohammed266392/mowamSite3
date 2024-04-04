37b91761d50901bdfc6c635a034cb802c0ba2057
raki <mohammed.raki26@gmail.com>
2024-04-04 17:09:46 +0200

    nos service details

--- src/app/directives/effect-test.directive.ts
+++ src/app/directives/effect-test.directive.ts
@@ -1,25 +1,25 @@
-import { Directive, ElementRef, HostListener, Input, OnDestroy, OnInit } from '@angular/core';
+import { Directive, ElementRef, HostListener, Input, OnDestroy, OnInit, Renderer2 } from '@angular/core';
 
 @Directive({
   selector: '[appEffectTest]',
   standalone: true
 })
 export class EffectTestDirective implements OnInit, OnDestroy {
-  @Input() backgroundColor: string = 'yellow';
-  @Input() backgroundColor1: string = 'blue';
-
+ 
   private observer?: IntersectionObserver;
 
-  constructor(private el: ElementRef) {}
+  constructor(private el: ElementRef, private renderer: Renderer2) {}
 
   ngOnInit(): void {
     this.observer = new IntersectionObserver(entries => {
       entries.forEach(entry => {
         console.log("mes datas", entry)
         if (entry.isIntersecting) {
-          this.el.nativeElement.style.backgroundColor = this.backgroundColor;
+          this.renderer.addClass(this.el.nativeElement,'swip-on-base')
+     
         }else{
-          this.el.nativeElement.style.backgroundColor = this.backgroundColor1;
+          this.renderer.removeClass(this.el.nativeElement,'swip-on-base')
+
         }
       });
     }, { threshold: 0.1 }); // Se déclenche lorsque 10% de l'élément est visible
