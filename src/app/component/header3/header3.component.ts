cb4f6fdb96a6be6b9699727b82792bc00a763211
raki <mohammed.raki26@gmail.com>
2024-04-19 13:23:24 +0200

    connection

--- src/app/component/header3/header3.component.ts
+++ src/app/component/header3/header3.component.ts
@@ -1,28 +1,24 @@
 import { Component, inject } from '@angular/core';
 import { OngletsPlusComponent } from '../tools/ongletsplus/ongletsplus.component';
-import { NgClass } from '@angular/common';
+import { NgClass, NgIf } from '@angular/common';
 import { OngletsplusMobile2Component } from '../tools/ongletsplus-mobile2/ongletsplus-mobile2.component';
-import { OngletService } from '../../services/onglet.service';
-import { Onglet } from '../../models/object';
+import { MatMenuModule } from '@angular/material/menu'
+import { MatIcon } from '@angular/material/icon'
 import { Router } from '@angular/router';
 import { AuthService } from '../../services/auth.service';
 
 @Component({
   selector: 'app-header3',
   standalone: true,
-  imports: [OngletsPlusComponent, NgClass, OngletsplusMobile2Component],
+  imports: [OngletsPlusComponent, NgClass, OngletsplusMobile2Component, MatMenuModule, MatIcon, NgIf],
   templateUrl: './header3.component.html',
   styleUrl: './header3.component.css'
 })
 export class Header3Component {
   router = inject(Router)
   auth = inject(AuthService)
-  connexion = this.auth.currentUser
+  currentUser = this.auth.currentUser
 
-  
-  getSomeThing(){
-    console.log("connexion1 : ",this.connexion())
-  }
 
   title : string = "MOWAM Solution"
   logo : string = "./../../assets/images/logo_solution_piscine.svg"
