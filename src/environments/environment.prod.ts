df83ba5a9e2f92ebf36c6f02f8fad4b811d087f6
mohammed266392 <mohammed.raki26@gmail.com>
2025-07-26 21:07:42 +0200

    show environment

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,5 @@
+export const environment = {
+  production: true,
+  apiGo: 'http://mowam-solution.de:30080',
+  appTodoList: 'http://mowam-solution.de:32201'
+};
---
+++ src/environments/environment.ts
@@ -0,0 +1,7 @@
+export const environment = {
+  production: false,
+  apiGo: 'http://localhost:8080',
+  appTodoList: 'http://localhost:4201'
+};
+
+//teste
